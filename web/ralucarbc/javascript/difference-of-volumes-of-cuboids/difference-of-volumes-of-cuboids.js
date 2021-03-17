908c205afbe0097de725367f4e5ef51ddc088cf1
RalucaRbc <office.movmil@gmail.com>
2021-03-17 14:05:48 +0000

    Refactor merge-2-sorted-arrays; Start difference-of-volumes-of-cuboids

---
+++ web/ralucarbc/javascript/difference-of-volumes-of-cuboids/difference-of-volumes-of-cuboids.js
@@ -0,0 +1,8 @@
+const divRead = document.querySelector("#read-input")
+const divFinal = document.querySelector("#write-output")
+
+let a = [2, 2, 3];
+let b = [5, 4, 1];
+
+divRead.textContent = "First cuboid's parameters are: [" + a + "] and second cuboid's parameters are: [" + b + "]"
+
