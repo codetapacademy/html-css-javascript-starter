1f03516462016ba5dd7df75750f1969ac9dc0a9a
RalucaRbc <office.movmil@gmail.com>
2021-03-18 22:45:36 +0000

    Display the used function for each feature

--- web/ralucarbc/javascript/sum-mixed-array/sum-mixed-array.js
+++ web/ralucarbc/javascript/sum-mixed-array/sum-mixed-array.js
@@ -1,8 +1,9 @@
-const divInstr = document.querySelector("#instructions");
+const h2Instr = document.querySelector("#instructions");
 const divContent = document.querySelector("#content");
 const divOutput = document.querySelector("#output");
+const divFunction = document.querySelector("#function");
 
-divInstr.textContent = `Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number.`;
+h2Instr.textContent = `Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number.`;
 
 let arr = ["5", "0", 9, 3, 2, 1, "9", 6, 7];
 divContent.textContent = `The test array is array = [` + arr + `]`;
@@ -12,3 +13,6 @@ divOutput.textContent =
   `The output (sum of the array values as if all were numbers) is: ` +
   sumMix(arr) +
   `.`;
+
+divFunction.textContent =
+  `The function that does this is const sumMix = ` + sumMix;
