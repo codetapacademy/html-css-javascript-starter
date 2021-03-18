5f605c5fc32681ca0654b2f4dd3918e2d50492bc
RalucaRbc <office.movmil@gmail.com>
2021-03-18 08:14:31 +0000

    feat: difference of 2 cuboids regardless of which is bigger; this closes #44

--- web/ralucarbc/javascript/difference-of-volumes-of-cuboids/difference-of-volumes-of-cuboids.js
+++ web/ralucarbc/javascript/difference-of-volumes-of-cuboids/difference-of-volumes-of-cuboids.js
@@ -1,8 +1,20 @@
-const divRead = document.querySelector("#read-input")
-const divFinal = document.querySelector("#write-output")
+const divRead = document.querySelector("#read-input");
+const divFinal = document.querySelector("#write-output");
 
 let a = [2, 2, 3];
 let b = [5, 4, 1];
 
-divRead.textContent = "First cuboid's parameters are: [" + a + "] and second cuboid's parameters are: [" + b + "]"
+divRead.textContent =
+  "First cuboid's parameters are: [" +
+  a +
+  "] and second cuboid's parameters are: [" +
+  b +
+  "]";
 
+const diffe = (a, b) =>
+  Math.abs(a.reduce((a, c) => a * c, 1) - b.reduce((a, c) => a * c, 1));
+
+divFinal.textContent =
+  "The difference of the cuboids' volumes regardless of which is bigger is: " +
+  diffe(a, b) +
+  ".";
