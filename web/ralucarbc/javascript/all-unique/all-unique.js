5a4302e2d9a31f641adb3a92807d6e761b64e634
RalucaRbc <office.movmil@gmail.com>
2021-03-18 22:23:25 +0000

    feat: all unique this closes #48

---
+++ web/ralucarbc/javascript/all-unique/all-unique.js
@@ -0,0 +1,15 @@
+const h2Instructions = document.querySelector("#instructions");
+const divContent = document.querySelector("#content");
+const divOutput = document.querySelector("#output");
+
+h2Instructions.textContent = `Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise. The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.`;
+
+let string = "++-";
+divContent.textContent = `The test string is: "` + string + `".`;
+
+const hasUniqueChars = (str) =>
+  str.length === [...new Set(str.split(""))].length ? true : false;
+divOutput.textContent =
+  `The output (true/false if a string does/does not contain only unique characters) is: ` +
+  hasUniqueChars(string) +
+  `.`;
