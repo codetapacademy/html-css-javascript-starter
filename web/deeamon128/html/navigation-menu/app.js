bf778f6c6923efe2d7a447278ec38bfd57d8a6e2
Your Name <andreeambobariu@gmail.com>
2021-03-30 20:45:20 +0100

    removed the commas and changed the grid ratio

--- web/deeamon128/html/navigation-menu/app.js
+++ web/deeamon128/html/navigation-menu/app.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('#mobile-menu');
+const menu = document.querySelector ('#mobile-menu');
 const menuLinks = document.querySelector ('.nav-menu');
 
 menu.addEventListener('click', function() {
