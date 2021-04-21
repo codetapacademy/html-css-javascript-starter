c9ef2a3d96331421e5fc920b5df9a3b24b9fa81c
Your Name <andreeambobariu@gmail.com>
2021-04-21 22:58:18 +0100

    progress bar

--- web/deeamon128/javascript/music-player/main.js
+++ web/deeamon128/javascript/music-player/main.js
@@ -1,4 +1,4 @@
-const musicContainer = document.querySelector('.music-container');
+const musicContainer = document.querySelector('#music-container');
 const prevBtn = document.querySelector('#prev');
 const playBtn = document.querySelector('#play');
 const nextBtn = document.querySelector('#next');
@@ -8,12 +8,13 @@ const musicCover = document.querySelector('#music-cover');
 const progressContainer = document.querySelector('.progress-container');
 const progress = document.querySelector('.progress');
 const musicTitle = document.querySelector('#music-title');
+console.log(musicContainer, playBtn)
 
 // Song Titles
 const songs = ['Gone', 'Love', 'Deepness']
 
 // Keep track of the songs
-let songIndex = 2
+let songIndex = 0
 
 // Initially load song info DOM
 loadSong(songs[songIndex]);
@@ -28,7 +29,7 @@ function loadSong(song) {
 // Add event listeners 
 playBtn.addEventListener('click', function() {
   const isPlaying = musicContainer.classList.contains('play')
-
+  console.log('play', isPlaying)
   if (isPlaying) {
     pauseSong()
   }
@@ -48,7 +49,7 @@ function playSong() {
 function pauseSong() {
   musicContainer.classList.remove('play')
   playBtn.querySelector('i.fas').classList.add('fa-play')
-  playBtn.querySelector('i-fas').classList.remove('fa-pause')
+  playBtn.querySelector('i.fas').classList.remove('fa-pause')
 
   audio.pause()
 }
@@ -81,4 +82,14 @@ function nextSong() {
   loadSong(songs[songIndex]);
 
   playSong();
+}
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
