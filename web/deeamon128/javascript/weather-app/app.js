66eaa61acb4d8187f995276666e365677f6019d3
Your Name <andreeambobariu@gmail.com>
2021-05-09 03:10:31 +0100

    fetch data with API

--- web/deeamon128/javascript/weather-app/app.js
+++ web/deeamon128/javascript/weather-app/app.js
@@ -1,14 +1,10 @@
 window.addEventListener("load", () => {
-  let long;
-  let lat;
   let locationTimeZone = document.querySelector('.location-timezone');
   let temperatureDegree = document.querySelector('.temperature-degree');
   let temperatureDescription = document.querySelector('.temperature-description');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
 
       const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?unitGroup=uk&key=E9R3A3G5GD6CPRK8S7YZBLCVB`;
       
@@ -17,13 +13,10 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then(data => {
-          console.log(data);
           const {conditions, temp} = data.currentConditions
-
-          temperatureDegree.textContent = temp
-          temperatureDescription.textContent = conditions
-          locationTimeZone = data.resolvedAddress
-          console.log(temperatureDescription)
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = conditions;
+          locationTimeZone.textContent = data.timezone
         })
     });
   }
